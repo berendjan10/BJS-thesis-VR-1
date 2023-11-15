ed3a5c97425e6f755ea60e0b97c73ab25969e429
Berend-Jan <berendjan10@live.nl>
2023-11-15 18:18:43 +0100

    the hip is locked on a predefined position on the chair. The spine rotates along with the head. I think I achieved what I was trying to accomplish

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -29,8 +29,16 @@ public class AvatarController : MonoBehaviour
     [SerializeField] Transform ikHead;
     [SerializeField] Vector3 headBodyOffset;
 
+    [SerializeField] Transform hipAnchor;
+    [SerializeField] Transform hipPosition;
+    [SerializeField] private Transform spine;
+    [SerializeField] Vector3 hipRotationOffset;
+    [SerializeField] Transform headReference;
+
+
     private void LateUpdate()
     {
+        // The transform keyword in Unity refers to the transform component of the game object that the script is attached to. In this case, the transform keyword is used to set the position of the game object that the AvatarController script is attached to
         transform.position = ikHead.position + headBodyOffset;
         transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(ikHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
 
@@ -38,6 +46,20 @@ public class AvatarController : MonoBehaviour
         leftHand.VRMapping();
         rightHand.VRMapping();
 
-    }
 
+        hipPosition.position = hipAnchor.position;
+        hipPosition.rotation = hipAnchor.rotation;
+
+        // calculate & set spine rotation
+        // Vector3 spineDirection = head.ikTarget.position - hipAnchor.position;
+        //Vector3 spineDirection = headReference.position - hipAnchor.position;
+        Vector3 spineDirection = headReference.position - hipAnchor.position;
+        // spineDirection = Quaternion.Euler(hipRotationOffset) * spineDirection;
+        Quaternion spineRotation = Quaternion.LookRotation(spineDirection, Vector3.forward);
+        spine.rotation = spineRotation * Quaternion.Euler(hipRotationOffset);
+
+        //// method 2
+        //float spine_length = Vector3.Distance(head.ikTarget.position, hipAnchor.position);
+        //spine.rotation = Quaternion.Euler(Mathf.Asin((head.ikTarget.position.x - hipAnchor.position.x) / spine_length), 0, Mathf.Asin((head.ikTarget.position.z - hipAnchor.position.z) / spine_length));
+    }
 }
